59e86d83930be2bb2bd1eb0c47d939a13e1f84a6
ー 灰桜 ー <hhbilly99@gmail.com>
2020-02-19 13:24:20 +0800

    🛠 Support CentOS 7.

--- README.md
+++ README.md
@@ -17,6 +17,7 @@ Setup in and run `app.js`  , just like
 
 ```bash
 $ npm i # or use 'yarn'
+$ chmod +x runcheck.sh
 $ node app.js
 ```
 
@@ -38,6 +39,8 @@ SSLChecker
      └─index.html
 ```
 **Notice: This tool can not work on Windows !**
+
+**Notice: The default Config is for Ubuntu 18.04. If you are using CentOS 7, please edit `runcheck.sh` .**
 
 ## Author
 
--- runcheck.sh
+++ runcheck.sh
@@ -8,25 +8,31 @@ cat ./tmp/ca.info | grep 'issuer: ' >> ./tmp/${1}.info
 cat ./tmp/ca.info | grep 'SSL certificate verify' >> ./tmp/${1}.info
 cat ./tmp/ca.info | grep 'subject: ' >> ./tmp/${1}.info
 
-# Ubuntu
-# sed -i 's|\* [\t]* start date: ||g' ./tmp/${1}.info
-# sed -i 's|\* [\t]* expire date: ||g' ./tmp/${1}.info
-# sed -i 's|\* [\t]* issuer: ||g' ./tmp/${1}.info
-# sed -i 's|\* [\t]* SSL certificate verify ||g' ./tmp/${1}.info
-# sed -i 's|\* [\t]* subject: ||g' ./tmp/${1}.info
+# Ubuntu 18.04
+sed -i 's|\* [\t]* start date: ||g' ./tmp/${1}.info
+sed -i 's|\* [\t]* expire date: ||g' ./tmp/${1}.info
+sed -i 's|\* [\t]* issuer: ||g' ./tmp/${1}.info
+sed -i 's|\* [\t]* SSL certificate verify ||g' ./tmp/${1}.info
+sed -i 's|\* [\t]* subject: ||g' ./tmp/${1}.info
 
-# CentOS
-sed -i 's/\* \tstart date: //g' ./tmp/${1}.info
-sed -i 's/\* \texpire date: //g' ./tmp/${1}.info
-sed -i 's/\* \tissuer: //g' ./tmp/${1}.info
-sed -i 's/\* \tSSL certificate verify //g' ./tmp/${1}.info
-sed -i 's/\* \tsubject: //g' ./tmp/${1}.info
+# Aliyun CentOS 7
+# sed -i 's/\* \tstart date: //g' ./tmp/${1}.info
+# sed -i 's/\* \texpire date: //g' ./tmp/${1}.info
+# sed -i 's/\* \tissuer: //g' ./tmp/${1}.info
+# sed -i 's/\* \tSSL certificate verify //g' ./tmp/${1}.info
+# sed -i 's/\* \tsubject: //g' ./tmp/${1}.info
 
 start=$(sed -n '1p' ./tmp/${1}.info)
 expire=$(sed -n '2p' ./tmp/${1}.info)
 issuer=$(sed -n '3p' ./tmp/${1}.info)
+
+# Ubuntu
 status=$(sed -n '4p' ./tmp/${1}.info)
 subject=$(sed -n '5p' ./tmp/${1}.info)
+
+# Aliyun CentOS 7
+# subject=$(sed -n '4p' ./tmp/${1}.info)
+
 
 rm -f ./tmp/ca.info
 rm -f ./tmp/${1}.info
@@ -36,12 +42,10 @@ DATE="$(echo $(date '+%Y-%m-%d %H:%M:%S'))"
 nowstamp="$(date -d "$DATE" +%s)"
 expirestamp="$(date -d "$expire" +%s)"
 
-# Ubuntu
-# expireday=$((expirestamp-nowstamp))
-# expireday=$((expireday/86400))
+expireday=$((expirestamp-nowstamp))
+expireday=$((expireday/86400))
 
-# CentOS
-expireday=`expr $[expirestamp-nowstamp] / 86400`
+# expireday=`expr $[expirestamp-nowstamp] / 86400`
 
 echo '{' > tmp/${1}.json
 echo '"domain": "'${1}'",' >> ./tmp/${1}.json
@@ -52,6 +56,7 @@ echo '"issuer": "'$issuer'",' >> ./tmp/${1}.json
 echo '"check": "'$DATE'",' >> ./tmp/${1}.json
 echo '"remain": "'$expireday'",' >> ./tmp/${1}.json
 
+# Ubuntu
 if [ $expirestamp \< $nowstamp ]; then
     echo '"status": "Expired",' >> ./tmp/${1}.json
     echo '"statuscolor": "error"' >> ./tmp/${1}.json
@@ -65,6 +70,18 @@ else
     echo '"status": "Invalid",' >> ./tmp/${1}.json
     echo '"statuscolor": "error"' >> ./tmp/${1}.json
 fi
+
+# Aliyun CentOS 7
+# if [ $expirestamp \< $nowstamp ]; then
+#     echo '"status": "Expired",' >> ./tmp/${1}.json
+#     echo '"statuscolor": "error"' >> ./tmp/${1}.json
+# elif [ $expireday \< 10 ]; then
+#     echo '"status": "Soon Expired",' >> ./tmp/${1}.json
+#     echo '"statuscolor": "warning"' >> ./tmp/${1}.json
+# else
+#     echo '"status": "Valid",' >> ./tmp/${1}.json
+#     echo '"statuscolor": "success"' >> ./tmp/${1}.json
+# fi
 
 echo '}' >> ./tmp/${1}.json
 
